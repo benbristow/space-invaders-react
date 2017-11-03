734c81388e0a4dd9bc5d353cbdb5432c0389a870
Ben Bristow <me@benbristow.co.uk>
2017-11-03 14:47:22 +0000

    Change public assets path again

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -58,7 +58,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: "assets/",
+  publicPath: "/space-invaders-react/assets/",
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
